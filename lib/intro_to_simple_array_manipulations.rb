f52687d6c2cf425a5a58626c56c90e8d52dd160c
James Hayward <james.hayward6@icloud.com>
2019-04-22 01:06:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,8 +20,8 @@ def using_shift(array)
   array.shift
 end
 
-def shift_with_args(array, num)
-  array.shift(num)
+def shift_with_args(array)
+  array.shift(2)
 end
 
 def using_concat(array1, array2)
